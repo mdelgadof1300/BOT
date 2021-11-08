af5cebcbd9e0bb8a8372ac463e1291d8075e771a
Eric Uriel Trejo Trejo (Erictoman) <ericto117@gmail.com>
2021-11-08 15:55:11 -0600

    Update index.js
    Nueva propuesta

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ bot.start((ctx) =>
     "Bienvenido, usa el comando /pregunta <pregunta> para cualquiera de tus inquietudes"
   )
 );
+
 bot.help((ctx) => ctx.reply("Send me a sticker"));
 bot.on("sticker", (ctx) => ctx.reply("👍"));
 bot.hears("hi", (ctx) => ctx.reply("Hey there"));
@@ -20,7 +21,7 @@ bot.hears(regex, async (ctx) => {
   if (data["answers"][0].score > 60) {
     ctx.reply(respuesta);
   } else {
-    ctx.reply("No tengo una respuesta certera, lo siento :c");
+    ctx.reply("No tengo una respuesta certera, lo siento :c.");
   }
 });
 bot.launch();
