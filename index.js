bdc28c58c964d1e76bec5645bd7aea551b9a8be7
Eric <ericto117@gmail.com>
2021-11-24 18:59:26 -0600

    N version

--- index.js
+++ index.js
@@ -26,6 +26,14 @@ bot.hears(regex, async (ctx) => {
 });
 bot.launch();
 
-// Enable graceful stop
-process.once("SIGINT", () => bot.stop("SIGINT"));
-process.once("SIGTERM", () => bot.stop("SIGTERM"));
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
